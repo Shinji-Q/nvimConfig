fb75f5316c5565a56e13391ef4b0225258d240fd
Pedro Shinji Endo Queiroz <pedro.queiroz@netbiis.com>
2023-09-15 12:25:27 -0300

    plugins update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,11 +3,6 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
 
 	use 'folke/tokyonight.nvim'
 	use {
@@ -55,4 +50,5 @@ return require('packer').startup(function(use)
 
 	use "windwp/nvim-autopairs"
 
+	use "iamcco/markdown-preview.nvim"
 end)
