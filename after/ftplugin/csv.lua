fdb159dbf9f0c1a82491ce0e3a6645ff6b62ed17
o-vento <shinjiendo37@gmail.com>
2025-12-21 20:00:28 -0300

    :zap: added bindings for differente separators

--- after/ftplugin/csv.lua
+++ after/ftplugin/csv.lua
@@ -1,1 +1,4 @@
-vim.keymap.set("n", "<leader>cv", "<CMD>CsvViewToggle<CR>", {buffer=true})
+vim.keymap.set("n", "<leader>cv", "<CMD>CsvViewToggle<CR>", { buffer = true })
+vim.keymap.set("n", "<leader>ct", "<CMD>CsvViewToggle delimiter=\\t<CR>", { buffer = true })
+vim.keymap.set("n", "<leader>c|", "<CMD>CsvViewToggle delimiter=|<CR>", { buffer = true })
+vim.opt_local.expandtab = false
